a0631788a873d175daaa4a8ec8c69e5eeaef8ab8
Ben Leggett <benjamin.leggett1@t-mobile.com>
2018-01-16 13:44:57 -0500

    Got one-by-one rolling working, at least

--- fistfulodice.go
+++ fistfulodice.go
@@ -10,13 +10,14 @@ import (
 func main() {
 
 	if len(os.Args) > 1 {
-		fmt.Printf("Rolling...\n")
 		count, err := strconv.Atoi(os.Args[1])
 		if err != nil {
 			fmt.Printf("Error parsing CLI: %s", err)
 		}
+		c := make(chan int, count)
 		for i := 0; i < count; i++ {
-			die.Roll(6)
+			die.Roll(6, c)
+			fmt.Printf("Result: %d\n", <- c)
 		}
 	} else {
 		fmt.Print("You must specify a number of dice to roll...\n")
